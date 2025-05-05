2bb83c7ac313094ada06cb45ebbd231e1c271bc8
FangyangJz <fangyang.jing@hotmail.com>
2025-05-05 18:16:51 +0800

    [Update] download bars script

--- examples/download_bars/download_bars.py
+++ examples/download_bars/download_bars.py
@@ -13,13 +13,15 @@ datafeed: TqsdkDatafeed = get_datafeed()
 database = get_database()
 cur_path = Path(__file__).parent
 
-interval = Interval.HOUR
+interval = Interval.MINUTE
 # df = datafeed.query_free_all(ind_class="CONT", interval=interval, data_length=1e4)
-# datafeed.save_df(df=df, file_path=cur_path / "cont_df.parquet")
-df = pl.read_parquet(cur_path / "cont_df.parquet")
-df = datafeed.format_df(df)
+# datafeed.save_df(df=df, file_path=cur_path / f"cont_{interval.value}_df.parquet")
+
+df = pl.read_parquet(cur_path / f"cont_{interval.value}_df.parquet")
 early_df = datafeed.get_early_pro_data(
-    cur_df=df, interval=interval, start_datetime=dt.datetime(2010, 1, 1)
+    cur_df=df, interval=interval, start_datetime=dt.datetime(2016, 1, 1)
 )
 df = pl.concat([early_df, df]).sort(["jj_code", "open_time"])
-datafeed.save_df(df=df, file_path=cur_path / "cont_2010_now_df.parquet")
+datafeed.save_df(
+    df=df, file_path=cur_path / f"cont_2010_now_{interval.value}_df.parquet"
+)
